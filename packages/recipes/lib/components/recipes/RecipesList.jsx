fe32bb31e5159b30fb894fb5b89a65ff26847964
Luis de Llano <kehd07@gmail.com>
2020-05-01 18:51:07 +0200

    Home and recipes same page #9

--- packages/recipes/lib/components/recipes/RecipesList.jsx
+++ packages/recipes/lib/components/recipes/RecipesList.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import { registerComponent, Components, withList, withCurrentUser, Loading } from 'meteor/vulcan:core';
-import Button from '@material-ui/core/Button';
 import Recipes from '../../modules/recipes/collection.js';
 
 const RecipesList = ({results = [], currentUser, loading}) => {
   return (
-    <div>
-      <Button>Spring</Button>
-      <Button>Summer</Button>
-      <Button>Fall</Button>
-      <Button>Winter</Button>
+    <div className="recipes-list">
+      {loading ?
+        <Loading /> :
+        <div className="recipes-list-content">
+          <div className="recipes-list-grid">
+            {results.map(recipe => <Components.RecipesItem key={recipe._id} recipe={recipe} currentUser={currentUser} />)}
+          </div>
+        </div>
+      }
     </div>
+
   );
 };
 
